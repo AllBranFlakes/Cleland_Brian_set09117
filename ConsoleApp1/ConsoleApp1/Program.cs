9b7f6a20302fb7a14cb4e1a67b7f5d695c20d777
Bran Flakes <branflakes@ymail.com>
2017-11-10 00:23:36 +0000

    // the almost everything bar AI commit \\ +has undo/redo +has save +has load
    -all new functions are buggy

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,11 +6,9 @@ using System.Text;
 
 /*
 TO-DO
-Version 0.8.4
+Version 0.8.5
 
 -needs AI
--needs save game function
--needs load game function
 */
 
 
@@ -20,23 +18,14 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            // Set title and console window display 
+            //set title and console window display 
             Console.Title = "G.L.A.D.O.S.";
             Console.BackgroundColor = ConsoleColor.White;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
             Console.SetWindowSize(110, 35);
 
-            // define the board structure (default value for a given board space is 0 - empty)
-            /*
-             In order to properly map the game pieces a state for each 'square' on the board is defined as:
-                0 - empty
-                1 - occupied (Red)
-                2 - occupied (Black)
-                3 - occupied (Red King)
-                4 - occupied (Black King)
-             */
-
+            //define the board structure (0 = empty | 1 = Red | 2 = Black | 3 = Red King | 4 = Black King)
             int[,] board =  { { 0, 1, 0, 1, 0, 1, 0, 1 },
                              { 1, 0, 1, 0, 1, 0, 1, 0 },
                              { 0, 1, 0, 1, 0, 1, 0, 1 },
@@ -46,38 +35,47 @@ namespace ConsoleApplication1
                              { 0, 2, 0, 2, 0, 2, 0, 2 },
                              { 2, 0, 2, 0, 2, 0, 2, 0 } };
 
-            /* define variables for cursor movement */
-            // define start position (x offset by +3 & y offset by +1 to account for the board layout)
+            //define start position (x offset by +3 & y offset by +1 to account for the board layout)
             int x = 4;
             int y = 1;
-            // variables for moving through the board
+
+            //variables for moving through the board
             int moveX = 0;
             int moveY = 0;
-            // define variables for locating pieces in board array (remember to adjust offsets so you dont fly off the end of the array!!)
+
+            //define variables for locating pieces in board array (remember to adjust offsets so you dont fly off the end of the array!!)
             int boardColumn = 0;
             int boardRow = 0;
-            // variables used to define the offsets that need to be adjusted as per previous comments
+
+            //variables used to define the offsets that need to be adjusted as per previous comments
             int boardArrayX = 0;
             int boardArrayY = 0;
             bool hop = false;
-            /* Various Variables */
-            // int for held pieces
+
+            //int for held pieces
             int holding = 0;
-            // int for turn
+
+            //int for turn
             int turn = 1;
-            // variables for move list (used for undo/redo and comparison of origin square versus destination square)
+
+            //variables for move list (used for undo/redo and comparison of origin square versus destination square)
             int[] origXY = { 0, 0 };
             int[] destXY = { 0, 0 };
+
             // player score variables
             int player1score = 0;
             int player2score = 0;
-            //move list
-            Dictionary<int, int[,]> moveList = new Dictionary<int, int[,]>();
 
+            //move list 
+            Dictionary<int, int[,]> moveList = new Dictionary<int, int[,]>
+            {
+                { 1, (int[,])board.Clone() }
+            };
+
+            //redo board
             int[,] redoMove = new int[8, 8];
 
-            moveList.Add(1, (int[,])board.Clone());
-            //statelist
+            //state lists
             int[] states = new int[3];
             int[] redoStates = new int[3];
 
@@ -118,9 +116,52 @@ namespace ConsoleApplication1
                     {
                         using (StreamWriter outputFile = new StreamWriter(@"C:\Users\Home\Documents\CheckersSave.csv"))
                         {
+                            //outputFile.WriteLine(turn);
+                            //outputFile.WriteLine(player1score);
+                            //outputFile.WriteLine(player2score);
                             foreach (KeyValuePair<int, int[,]> pair in moveList)
                             {
-                                outputFile.WriteLine(pair.Key + "," + player1score + "," + player2score + "," + String.Join(",",pair.Value.Cast<int>())); 
+                                outputFile.WriteLine(String.Join(",", pair.Value.Cast<int>()));
+                            }
+                        }
+                    }
+
+                    if (key == ConsoleKey.L)
+                    {
+
+                        using (StreamReader sr = new StreamReader(@"C:\Users\Home\Documents\CheckersSave.csv"))
+                        {
+                            int[,] newRow = new int[8, 8];
+                            var lineCount = File.ReadLines(@"C:\Users\Home\Documents\CheckersSave.csv").Count();
+                            string line;
+                            int moveCount = 0;
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                while (moveCount != lineCount)
+                                {
+                                    int[] ia = line.Split(',').Select(n => Convert.ToInt32(n)).ToArray();
+                                    int counter = 0;
+                                    while (counter < 64)
+                                    {
+                                        for (int j = 0; j < 8; j++)
+                                        {
+                                            for (int k = 0; k < 8; k++)
+                                            {
+                                                newRow[j, k] = ia[counter];
+                                                counter++;
+                                            }
+                                        }
+                                    }
+                                    if (moveList.ContainsKey(moveCount) == true)
+                                    {
+                                        moveList[moveCount] = (int[,])newRow.Clone();
+                                    }
+                                    else
+                                    {
+                                        moveList.Add(moveCount, newRow);
+                                    }
+                                    moveCount++;
+                                }
                             }
                         }
                     }
@@ -492,7 +533,7 @@ namespace ConsoleApplication1
                     {
                         Console.ForegroundColor = ConsoleColor.DarkGreen;
                         Console.SetCursorPosition(1, 18);
-                        Console.Write(" ->");
+                        Console.Write("->");
                         Console.SetCursorPosition(1, 19);
                         Console.Write("   ");
                     }
@@ -593,12 +634,12 @@ namespace ConsoleApplication1
 
             Console.WriteLine("  ╔═══╦═══╦═══╦═══╦═══╦═══╦═══╦═══╗");
             Console.WriteLine("  ║   ║░░░║   ║░░░║   ║░░░║   ║░░░║         ╔════════════════════════════════════════════════════════╗");
-            Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣         ║  Grid Like Arrayed Draughts Organizing System v:0.8.4  ║");
+            Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣         ║  Grid Like Arrayed Draughts Organizing System v:0.8.5  ║");
             Console.WriteLine("  ║░░░║   ║░░░║   ║░░░║   ║░░░║   ║         ╚════════════════════════════════════════════════════════╝");
             Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣            - Move the cursor with the arrow keys.");
             Console.WriteLine("  ║   ║░░░║   ║░░░║   ║░░░║   ║░░░║            - Press space to select/move.");
-            Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣            - Press U to undo your move");
-            Console.WriteLine("  ║░░░║   ║░░░║   ║░░░║   ║░░░║   ║            - Press R to redo your move");
+            Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣            - Press U to undo or R to redo your move");
+            Console.WriteLine("  ║░░░║   ║░░░║   ║░░░║   ║░░░║   ║            - Press S to save or L to load");
             Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣");
             Console.WriteLine("  ║   ║░░░║   ║░░░║   ║░░░║   ║░░░║");
             Console.WriteLine("  ╠═══╬═══╬═══╬═══╬═══╬═══╬═══╬═══╣            - Select a piece.");
